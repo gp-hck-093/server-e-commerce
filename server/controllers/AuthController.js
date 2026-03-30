e22e3ade4d6ff5c0228394ada49605915cd041a3
raturamadhani01 <raturmn798@gmail.com>
2026-03-30 23:23:10 +0700

    feat: implement Google login functionality in AuthController

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -1,3 +1,4 @@
+const { OAuth2Client } = require("google-auth-library")
 const {User} = require("../models")
 const {comparePassword} = require("../helpers/bcrypt")
 const {generateToken} = require("../helpers/jwt")
@@ -44,6 +45,41 @@ class AuthController {
 
   static async googleLogin(req, res, next) {
     try {
+      const { credential } = req.body
+      if (!credential) throw { name: "BadRequest", message: "Google credential is required" }
+
+      const audience = process.env.GOOGLE_CLIENT_ID
+      if (!audience) throw { name: "BadRequest", message: "GOOGLE_CLIENT_ID is not set" }
+
+      const client = new OAuth2Client(audience)
+      const ticket = await client.verifyIdToken({ idToken: credential, audience })
+      const payload = ticket.getPayload()
+      const { email, name, sub } = payload
+
+      if (!email) throw { name: "Unauthorized", message: "Google account email not found" }
+
+      let user = await User.findOne({ where: { email } })
+
+      if (!user) {
+        const randomPassword = `Google-${Math.random().toString(36).slice(-8)}A1!`
+        user = await User.create({
+          username: name || email.split("@")[0],
+          email,
+          password: randomPassword,
+          googleId: sub || null,
+          phoneNumber: "-",
+          address: "-"
+        })
+      } else if (!user.googleId && sub) {
+        await user.update({ googleId: sub })
+      }
+
+      const access_token = generateToken({
+        id: user.id,
+        email: user.email,
+      })
+
+      res.status(200).json({ access_token })
     } catch (error) {
       next(error);
     }
