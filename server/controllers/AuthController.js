4f4b02c7c687370aac5faedad2a304ffec78a074
raturamadhani01 <raturmn798@gmail.com>
2026-03-30 20:11:10 +0700

    feat: implement user registration in AuthController

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -1,6 +1,16 @@
 class AuthController {
   static async register(req, res, next) {
     try {
+      const { username, email, password, googleId, phoneNumber, address } = req.body
+      const data = await User.create({ username, email, password, googleId, phoneNumber, address })
+      res.status(201).json({
+        id: data.id,
+        username: data.username,
+        email: data.email,
+        googleId: data.googleId,
+        phoneNumber: data.phoneNumber,
+        address: data.address
+      })
     } catch (error) {
       next(error);
     }
@@ -8,6 +18,7 @@ class AuthController {
 
   static async login(req, res, next) {
     try {
+    
     } catch (error) {
       next(error);
     }
