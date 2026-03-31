b6bb17973addff717d448083c3681b357c16c60a
raturamadhani01 <raturmn798@gmail.com>
2026-03-31 09:15:03 +0700

    feat: remove googleId from user registration response in AuthController

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -6,13 +6,12 @@ const {generateToken} = require("../helpers/jwt")
 class AuthController {
   static async register(req, res, next) {
     try {
-      const { username, email, password, googleId, phoneNumber, address } = req.body
-      const data = await User.create({ username, email, password, googleId, phoneNumber, address })
+      const { username, email, password, phoneNumber, address } = req.body
+      const data = await User.create({ username, email, password, phoneNumber, address })
       res.status(201).json({
         id: data.id,
         username: data.username,
         email: data.email,
-        googleId: data.googleId,
         phoneNumber: data.phoneNumber,
         address: data.address
       })
