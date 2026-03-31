f16476a818dd328cd11c9ddee6b75570b31fbee7
erwindW99 <erwindwiranata17@gmail.com>
2026-03-31 15:37:17 +0700

    chore: create category and brand controller

--- server/routes/products.js
+++ server/routes/products.js
@@ -8,4 +8,7 @@ router.get("/", ProductController.getAllProducts);
 // Get product detail
 router.get("/:id", ProductController.getProductById);
 
+router.get("/categories", ProductController.getCategories);
+router.get("/brands", ProductController.getBrands);
+
 module.exports = router;
